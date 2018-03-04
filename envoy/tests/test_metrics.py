c70e3894595c4f44d9e344fc8c9cf6d90f890d3e
Ofek Lev <ofekmeister@gmail.com>
2018-03-03 19:40:20 -0500

    test prefix

--- envoy/tests/test_metrics.py
+++ envoy/tests/test_metrics.py
@@ -1,5 +1,9 @@
-from datadog_checks.envoy.metrics import METRICS, METRIC_TREE
+from datadog_checks.envoy.metrics import METRIC_PREFIX, METRIC_TREE, METRICS
 from datadog_checks.envoy.utils import make_metric_tree
+
+
+def test_metric_prefix():
+    assert METRIC_PREFIX == 'envoy.'
 
 
 def test_metric_tree():
